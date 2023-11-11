c6e96b08a8f2fbe0a3ea70756880c74d8eda34bf
Harziz712 <109679217+Harziz712@users.noreply.github.com>
2023-11-11 08:39:05 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
+
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
+      showPopup(true);
+    }else{
+  alert("invalid input");
+    }
+}
+
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+  
+}
+
+let headerLinks
+toggleBtn = document.querySelector("#navlink-toggle")
+headerLinks = document.querySelector("#header-links")
+toggleBtn.onclick = () => {
+    if (headerLinks.style.display === "none") {
+        headerLinks.style.display = "block"
+    } else {
+        headerLinks.style.display = "none"
+    }
+}
+
