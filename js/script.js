db5661f1c512049092982a39af4259e02b42aa6e
Harziz712 <109679217+Harziz712@users.noreply.github.com>
2023-11-14 06:19:47 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,19 +13,74 @@ function showSlides() {
   setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
 
-function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
+let toggleBtn 
+let headerLinks
+let body
+body=  document.querySelector("#hero")
+toggleBtn = document.querySelector("#navlink_toggle")
+headerLinks = document.querySelector( ".navbar")
+
+toggleBtn.onclick = () => { 
+    if( headerLinks.style.display === "none" ){ 
+        headerLinks.style.display = "grid"
+    } else{ headerLinks.style.display = "none"}
+ }
+
+ body.onclick = () => { 
+    if( headerLinks.style.display === "grid" ){ 
+        headerLinks.style.display = "none"
+    }
+ }
+
+//   // Get the message of the new recommendation
+//   let recommendation = document.getElementById("new_recommendation");
+//  function addRecommendation() {
+
+//   // If the user has left a recommendation, display a pop-up
+//   if (recommendation.value != null && recommendation.value.trim() != "") {
+//     console.log("New recommendation added");
+//     //Call showPopup here
+
+ 
+//   showPopup(true);
+// }
+// else{
+//   alert("invalid input");
+// }
+// }
+
+
+// function showPopup(bool) {
+//   if (bool) {
+//     document.getElementById('popup').style.visibility = 'visible'
+//   } else {
+//     document.getElementById('popup').style.visibility = 'hidden'
+//   }
+  
+// }
+
+let newLetter = document.querySelector("#new_email");
+let popup = document.querySelector("#popup");
+let emailBtn = document.querySelector("#email_btn");
+let confirmBtn = document.querySelector("#confirmbtn");
+
+emailBtn.onclick = () => { 
+  if (newLetter.value != null && newLetter.value.trim() != "") {
     console.log("New recommendation added");
     //Call showPopup here
-      showPopup(true);
-    }else{
+
+ 
+  showPopup(true);
+}
+else{
   alert("invalid input");
-    }
 }
 
+}
+
+confirmBtn.onclick  = () => { 
+  popup.style.visibility = 'hidden'
+}
 
 function showPopup(bool) {
   if (bool) {
@@ -36,14 +91,4 @@ function showPopup(bool) {
   
 }
 
-let headerLinks
-toggleBtn = document.querySelector("#navlink-toggle")
-headerLinks = document.querySelector("#header-links")
-toggleBtn.onclick = () => {
-    if (headerLinks.style.display === "none") {
-        headerLinks.style.display = "block"
-    } else {
-        headerLinks.style.display = "none"
-    }
-}
 
